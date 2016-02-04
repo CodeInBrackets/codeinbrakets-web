61c96f756c1ded07e2cb8cb090e525accf629334
kanekotic <alvarojosepl@gmail.com>
2016-02-04 08:39:15 +0100

    improvements on style

--- server/methods.js
+++ server/methods.js
@@ -1,10 +1,10 @@
 Meteor.methods({
   togglePlaylistPublish: function(id, currentState){
     Playlists.update(id, {
-      $set:{
-        isPublic: !currentState
-      }
-    })
+        $set:{
+            isPublic: !currentState
+        }
+    });
   },
   deletePlaylist: function(id){
     Playlists.remove(id);
